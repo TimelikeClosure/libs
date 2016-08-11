49fad04d298577aaf087e00d978c57805282bd84
Tim Horist Jr <Tim Horist Jr>
2016-08-11 12:50:12 -0700

    cssbreakout - added base functionality for CSSBreakout configuration

---
+++ js/cssbreakout.js
@@ -0,0 +1,45 @@
+
+function CSSBreakout(rootElement) {
+
+    var config = {
+        recursive: false
+    };
+    /**
+     * Interface for configuration rules list. If no parameter is provided, returns a list of current config options
+     * @param {Object|undefined} ruleList
+     * @returns {Object|boolean}
+     */
+    this.config = function(ruleList){
+        if (ruleList === undefined) {
+            return copyObject(config);
+        }
+        for (var rule in ruleList){
+            if (ruleList.hasOwnProperty(rule) && config.hasOwnProperty(rule)){
+                config[rule] = ruleList[rule];
+            }
+        }
+    };
+
+    /**
+     * copyObject - returns a clone of the given object or array with no shared references
+     * @param {Object|Array} originalObject - reference object to clone
+     * @returns {Object|Array} clone of the given reference object
+     */
+    function copyObject(originalObject){
+        if (Array.isArray(originalObject)){
+            var copiedObject = [];
+        } else {
+            copiedObject = {};
+        }
+        for (var key in originalObject){
+            if (originalObject.hasOwnProperty(key)){
+                if (typeof originalObject[key] != "object" || originalObject[key] === null){
+                    copiedObject[key] = originalObject[key];
+                } else {
+                    copiedObject[key] = copyObject(originalObject[key]);
+                }
+            }
+        }
+        return copiedObject;
+    }
+}
